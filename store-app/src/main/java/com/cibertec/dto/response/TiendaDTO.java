1ca59c1585066f57db9a13c6ada049f0fbb8ea2b
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-10-28 10:37:42 -0500

    feat(store-app): agregar clases TiendaDTO, SuccessResponse y ErrorResponse en el paquete com.cibertec.dto.response
    Se implementaron las clases para
    estandarizar las respuestas de la API:
    - TiendaDTO: define los datos de la tienda.
    - SuccessResponse: estructura genérica para respuestas exitosas.
    - ErrorResponse: estructura para respuestas de error con detalles.

---
+++ store-app/src/main/java/com/cibertec/dto/response/TiendaDTO.java
@@ -0,0 +1,19 @@
+package com.cibertec.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TiendaDTO {
+	private Integer id;
+	private String rutaImagen;
+	private String nombre;
+	private String nombreLegal;
+	private String direccion;
+	private String ruc;
+	private String pais;
+	private String departamento;
+	private String provincia;
+	private String distrito;
+}
