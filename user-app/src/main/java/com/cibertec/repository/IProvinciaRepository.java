5eb17a60e25b8938061e9752d0117dfbecbecf0b
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-10-28 12:28:02 -0500

    eliminar repositorios inncesarios del user-app

--- user-app/src/main/java/com/cibertec/repository/IProvinciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cibertec.entity.Provincia;
-
-public interface IProvinciaRepository extends JpaRepository<Provincia, Integer> {
-
-}
