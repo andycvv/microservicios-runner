6cf743e202b668bd8e516226732a8149d742a0a3
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-10-29 16:23:16 -0500

    feat(store-app): ampliar interfaz TiendaService con nuevas operaciones CRUD
    - Se añadieron nuevos métodos a la interfaz TiendaService para gestionar tiendas:
      • listarTiendasActivas()
      • crearTienda(TiendaCreacionDTO)
      • actualizarTienda(Integer, TiendaActualizarDTO)
      • cambiarEstado(Integer)
      • eliminarTienda(Integer)
    - Estas nuevas operaciones permiten un manejo más completo del ciclo de vida de las tiendas.

--- store-app/src/main/java/com/cibertec/service/TiendaService.java
+++ store-app/src/main/java/com/cibertec/service/TiendaService.java
@@ -2,11 +2,18 @@ package com.cibertec.service;
 
 import java.util.List;
 
+import com.cibertec.dto.request.TiendaActualizarDTO;
+import com.cibertec.dto.request.TiendaCreacionDTO;
 import com.cibertec.dto.response.SuccessResponse;
 import com.cibertec.dto.response.TiendaDTO;
 
 public interface TiendaService {
 
 	public SuccessResponse<List<TiendaDTO>> listarTiendas();
+	public SuccessResponse<List<TiendaDTO>> listarTiendasActivas();
 	public SuccessResponse<TiendaDTO> obtenerTiendaPorId(Integer id);
+	public SuccessResponse<TiendaDTO> crearTienda(TiendaCreacionDTO tienda);
+	public SuccessResponse<TiendaDTO> actualizarTienda(Integer id, TiendaActualizarDTO tienda);
+	public SuccessResponse<String> cambiarEstado(Integer id);
+	public SuccessResponse<String> eliminarTienda(Integer id);
 }
