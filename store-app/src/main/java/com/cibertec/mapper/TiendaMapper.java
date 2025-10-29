def695afe035fc83bdb4adcf667747eb9404264d
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-10-29 16:20:19 -0500

    refactor(store-app): actualizar lógica de mapeo en TiendaMapper
    - Se simplificó el método toTiendaDTO eliminando los mapeos de entidades relacionadas
      (pais, departamento, provincia, distrito) y ajustando el uso de isEnabled.
    - Se mejoró el método toTienda(TiendaCreacionDTO) eliminando el parámetro id
      y completando la asignación de atributos.
    - Se agregó un nuevo método toTienda(Integer id, TiendaActualizarDTO)
      para mapear datos durante la actualización de tiendas.

--- store-app/src/main/java/com/cibertec/mapper/TiendaMapper.java
+++ store-app/src/main/java/com/cibertec/mapper/TiendaMapper.java
@@ -2,6 +2,8 @@ package com.cibertec.mapper;
 
 import org.springframework.stereotype.Service;
 
+import com.cibertec.dto.request.TiendaActualizarDTO;
+import com.cibertec.dto.request.TiendaCreacionDTO;
 import com.cibertec.dto.response.TiendaDTO;
 import com.cibertec.entity.Tienda;
 
@@ -15,12 +17,47 @@ public class TiendaMapper {
 				.nombreLegal(tienda.getNombreLegal())
 				.direccion(tienda.getDireccion())
 				.ruc(tienda.getRuc())
-				.pais(tienda.getPais().getNombre())
-				.departamento(tienda.getDepartamento().getNombre())
-				.provincia(tienda.getProvincia().getNombre())
-				.distrito(tienda.getDistrito().getNombre())
-				.isEnabled(tienda.getIsEnabled())
+				.isEnabled(tienda.isEnabled())
 				.build();
+	}
+	
+	public Tienda toTienda(TiendaCreacionDTO creacionDTO) {
+		Tienda tienda = new Tienda();
+		tienda.setNombre(creacionDTO.getNombre());
+		tienda.setNombreLegal(creacionDTO.getNombreLegal());
+		tienda.setDireccion(creacionDTO.getDireccion());
+		tienda.setRuc(creacionDTO.getRuc());
+		tienda.setRutaImagen(creacionDTO.getRutaImagen());
+		tienda.setUbicacion(creacionDTO.getUbicacion());
+		tienda.setTelefono(creacionDTO.getTelefono());
+		tienda.setMail(creacionDTO.getMail());
+		tienda.setIdPais(creacionDTO.getIdPais());
+		tienda.setIdDepartamento(creacionDTO.getIdDepartamento());
+		tienda.setIdDistrito(creacionDTO.getIdDistrito());
+		tienda.setIdProvincia(creacionDTO.getIdProvincia());
+		
+		return tienda;
+	}
+	
+	
+	public Tienda toTienda(Integer id, TiendaActualizarDTO actualizarDTO) {
+		Tienda tienda = new Tienda();
+		tienda.setId(id);
+		tienda.setNombre(actualizarDTO.getNombre());
+		tienda.setNombreLegal(actualizarDTO.getNombreLegal());
+		tienda.setDireccion(actualizarDTO.getDireccion());
+		tienda.setRuc(actualizarDTO.getRuc());
+		tienda.setRutaImagen(actualizarDTO.getRutaImagen());
+		tienda.setUbicacion(actualizarDTO.getUbicacion());
+		tienda.setTelefono(actualizarDTO.getTelefono());
+		tienda.setMail(actualizarDTO.getMail());
+		tienda.setIdPais(actualizarDTO.getIdPais());
+		tienda.setIdDepartamento(actualizarDTO.getIdDepartamento());
+		tienda.setIdDistrito(actualizarDTO.getIdDistrito());
+		tienda.setIdProvincia(actualizarDTO.getIdProvincia());
+		tienda.setEnabled(actualizarDTO.isEnabled());
+		
+		return tienda;
 	}
 	
 }
