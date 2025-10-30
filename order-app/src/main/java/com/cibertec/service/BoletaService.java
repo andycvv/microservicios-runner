8bc26736172318204167541f5f2f92556c962002
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-10-29 22:01:57 -0500

    feat(order-app): implementar servicio BoletaService con lógica de negocio dentro de mi paquete com.cibertec.service.implement
    Se creó la interfaz BoletaService y su implementación BoletaServiceImpl.
    Incluye métodos para listar boletas, buscar por trabajador o cliente, obtener por ID y crear nuevas boletas con sus transacciones.
    El método de creación utiliza Feign Client para obtener información del producto desde product-app.

---
+++ order-app/src/main/java/com/cibertec/service/BoletaService.java
@@ -0,0 +1,15 @@
+package com.cibertec.service;
+
+import java.util.List;
+
+import com.cibertec.dto.request.BoletaCreacionDTO;
+import com.cibertec.response.BoletaDTO;
+import com.cibertec.response.SuccessResponse;
+
+public interface BoletaService {
+	public SuccessResponse<List<BoletaDTO>> listarBoletas();
+	public SuccessResponse<List<BoletaDTO>> listarBoletasPorTrabajadorId(Integer id);
+	public SuccessResponse<List<BoletaDTO>> listarBoletasPorClienteId(Integer id);
+	public SuccessResponse<BoletaDTO> obtenerBoletaPorId(Integer id);
+	public SuccessResponse<BoletaDTO> crearBoleta(BoletaCreacionDTO creacionDTO);
+}
