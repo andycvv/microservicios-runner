a4971f2d750b7f56c4d8c37a4adad4f1b5cfa1e6
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-10-29 22:02:59 -0500

    refactor(order-app): actualizar repositorios JPA para boleta y transacción dentro de mi paquete com.cibertec.repository
    Se añadieron consultas personalizadas en IBoletaRepository y se modificó ITransaccionRepository
    para simplificar el uso de claves primarias eliminando el uso de TransaccionId embebido.

--- order-app/src/main/java/com/cibertec/repository/ITransaccionRepository.java
+++ order-app/src/main/java/com/cibertec/repository/ITransaccionRepository.java
@@ -3,8 +3,7 @@ package com.cibertec.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cibertec.entity.Transaccion;
-import com.cibertec.entity.TransaccionId;
 
-public interface ITransaccionRepository extends JpaRepository<Transaccion, TransaccionId> {
+public interface ITransaccionRepository extends JpaRepository<Transaccion, Integer> {
 
 }
