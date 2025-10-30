0201277d681cdcf1cc26110223a306ee27dd7e41
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-10-29 21:55:33 -0500

    feat: agregar DTOs de creación para boleta y transacción
    Se crearon los DTOs BoletaCreacionDTO y TransaccionCreacionDTO para manejar los datos de entrada
    en la creación de boletas y sus transacciones asociadas.

--- order-app/src/main/java/com/cibertec/dto/request/BoletaCreacionDTO.java
+++ order-app/src/main/java/com/cibertec/dto/request/BoletaCreacionDTO.java
@@ -1,8 +1,24 @@
 package com.cibertec.dto.request;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import lombok.Data;
 
 @Data 
 public class BoletaCreacionDTO {
-
+	private String direccion;
+	private String tipoTarjeta;
+	private BigDecimal totalDescuento;
+	private String nombreCliente;
+	private String numeroDocumento;
+	private BigDecimal subtotal;
+	private BigDecimal igv;
+	private BigDecimal total;
+	private String estado;
+	private String observaciones;
+	private Integer idUsuario;
+	private Integer idTienda;
+	private Integer idTrabajdor;
+	private List<TransaccionCreacionDTO> transacciones;
 }
